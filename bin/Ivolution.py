c6bbb12f8b4ae405a4bf0fa89c1d215e91c38974
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-07-19 11:09:42 +0200

    Moves startup script to bin folder

--- bin/Ivolution.py
+++ bin/Ivolution.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir) 
+
 from gi.repository import Gtk, GObject
 from gui import IvolutionWindow
 
