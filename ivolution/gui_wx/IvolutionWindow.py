730025393e24af9c4efe0aae76b57fa478d27338
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-08-29 18:37:39 +0200

    First version of about window. All is put inside the on_about method

--- ivolution/gui_wx/IvolutionWindow.py
+++ ivolution/gui_wx/IvolutionWindow.py
@@ -24,7 +24,6 @@ from .. import FacemovieThread
 from ..util.Notifier import Observer
 from ..util.Notifier import Observable
 
-from AboutDialog import AboutDialog
 from IvolutionTemplate import IvolutionTemplate
 
 
@@ -146,9 +145,40 @@ class IvolutionWindow(IvolutionTemplate, Observer, Observable):
         """
         Displays the about box for Ivolution
         """
-        about = AboutDialog(self, "Ivolution")
-        about.ShowModal()  # Show it
-        about.Destroy()  # finally destroy it when finished.
+        description = """File Hunter is an advanced file manager for
+the Unix operating system. Features include powerful built-in editor,
+advanced search capabilities, powerful batch renaming, file comparison,
+extensive archive handling and more.
+"""
+
+        licence = """File Hunter is free software; you can redistribute
+it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+File Hunter is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details. You should have
+received a copy of the GNU General Public License along with File Hunter;
+if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+Suite 330, Boston, MA  02111-1307  USA"""
+
+        info = wx.AboutDialogInfo()
+
+        #info.SetIcon(wx.Icon('hunter.png', wx.BITMAP_TYPE_PNG))
+        info.SetName('File Hunter')
+        info.SetVersion('1.0')
+        info.SetDescription(description)
+        info.SetCopyright('(C) 2007 - 2011 Jan Bodnar')
+        info.SetWebSite('http://www.zetcode.com')
+        info.SetLicence(licence)
+        info.AddDeveloper('Jan Bodnar')
+        info.AddDocWriter('Jan Bodnar')
+        info.AddArtist('The Tango crew')
+        info.AddTranslator('Jan Bodnar')
+
+        wx.AboutBox(info)
 
     def on_exit(self, event):
         """
