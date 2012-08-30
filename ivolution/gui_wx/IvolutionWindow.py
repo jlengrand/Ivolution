9365e63eaf9e974e4379939368d8d10648138419
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-08-30 18:10:32 +0200

    Tries to enhance about dialog

--- ivolution/gui_wx/IvolutionWindow.py
+++ ivolution/gui_wx/IvolutionWindow.py
@@ -149,23 +149,44 @@ class IvolutionWindow(IvolutionTemplate, Observer, Observable):
         """
         Displays the about box for Ivolution
         """
-        description = """FaceMovie is a project aiming at helping you
- create videos of yourself over time. Simply take pictures of yourself, Facemovie does everything else for you.
-FaceMovie may be used for faces, but also profiles (to show women along pregnancy for example) or full body (for people workouting). The only limitation comes from you !
+        description ="""    Ivolution is a project aiming at helping you
+create videos of yourself over time.
+    Simply take pictures of yourself, Ivolution does
+everything else for you.
+
+    Ivolution may be used for faces, but also profiles
+(to show women along pregnancy for example)
+or full body (for people workouting).
+
+    The only limitation comes from you !
 """
 
-        licence = """Ivolution is free software; you can redistribute
-it and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
+        licence = """Copyright (c) <2012>, <Julien Lengrand-Lambert>
+All rights reserved.
 
-File Hunter is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-See the GNU General Public License for more details. You should have
-received a copy of the GNU General Public License along with File Hunter;
-if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-Suite 330, Boston, MA  02111-1307  USA"""
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project."""
 
         info = wx.AboutDialogInfo()
 
