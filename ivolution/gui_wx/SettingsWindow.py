5eed8c66c28bace5bf5a136d845fdaf895c80e0e
Julien Lengrand-Lambert <jlengrand@gmail.com>
2012-08-31 10:00:42 +0200

    Implement on_output for settings.

--- ivolution/gui_wx/SettingsWindow.py
+++ ivolution/gui_wx/SettingsWindow.py
@@ -2,7 +2,7 @@
 """
 .. module:: SettingsWindow
    :platform: Unix, Windows, Mac
-   :synopsis: Settings Window of the Ivolution GUI designed to be supported by all platforms.
+   :synopsis: Settings Window of the Ivolution GUI.
 
 .. moduleauthor:: Julien Lengrand-Lambert <jlengrand@gmail.com>
 
@@ -10,11 +10,6 @@
 
 import wx
 import wx.lib.newevent
-
-import sys
-import os
-import logging
-import webbrowser
 
 from .. import get_data # used to load images and files
 
@@ -31,31 +26,35 @@ class SettingsWindow(SettingsTemplate):
         """
         SettingsTemplate.__init__(self, parent)
         # TODO : Set icon
-        #self.SetIcon(wx.Icon('ivolution/data/media/icons/spanner_48.ico', wx.BITMAP_TYPE_ICO))  # Sets icon
-
+        # Sets icon
+        #self.SetIcon(wx.Icon('ivolution/data/media/icons/spanner_48.ico',
+        #            wx.BITMAP_TYPE_ICO))
 
         # Defining settings value
         self.output_folder = parent.out_fo
-        self.file_name = "Ivolution"
+        self.video_name = "Ivolution"
+        self.type = ""
+        self.mode = ""
+        self.speed = ""
+        self.sort = ""
 
-
-
-
-
-        self.outputLocationLabel.SetLabel(self.output_folder) # setting default value from main window
+        # setting default value from main window
+        self.outputLocationLabel.SetLabel(self.output_folder)
 
     # Virtual event handlers, overide them in your derived class
-    def on_output( self, event ):
-        pass
-        # default_dir = "~/Pictures"
-        # self.inputdialog = wx.DirDialog(self, "Please choose your input directory", style=1, defaultPath=default_dir)
+    def on_output(self, event):
+        self.outputdialog = wx.DirDialog(self,
+                                        "Please choose your output directory",
+                                        style=1,
+                                        defaultPath=self.output_folder)
 
-        # if self.inputdialog.ShowModal() == wx.ID_OK:
-        #     self.inputtextbox.SetLabel(self.inputdialog.GetPath())
-        # self.inputdialog.Destroy()
+        if self.outputdialog.ShowModal() == wx.ID_OK:
+            self.output_folder = self.outputdialog.GetPath()
+            self.outputLocationLabel.SetLabel(self.output_folder)
+        self.outputdialog.Destroy()
 
-    def on_cancel( self, event ):
+    def on_cancel(self, event):
         self.Close(True)  # Close the frame.
 
-    def on_save( self, event ):
+    def on_save(self, event):
         event.Skip()
